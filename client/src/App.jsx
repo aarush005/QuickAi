a40a4e280d8eb997f3e0a4091a0549316d472fab
aarush005 <astaarush005@gmail.com>
2025-10-11 01:19:17 +0530

    Initial Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,34 @@
+import  Home  from './pages/Home'
+import { Route, Routes } from 'react-router-dom'
+import Layout from './pages/Layout'
+import Dashboard from './pages/Dashboard'
+import BlogTitles from './pages/BlogTitles'
+import WriteArticle from './pages/WriteArticle'
+import GenerateImages from './pages/GenerateImages'
+import RemoveBackground from './pages/RemoveBackground'
+import RemoveObjects from './pages/RemoveObjects'
+import { ReviewResume } from './pages/ReviewResume'
+import Community from './pages/Community'
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/ai' element={<Layout/ >}>
+          <Route index element={<Dashboard/>}/>
+          <Route path='write-article' element={<WriteArticle/>}/>
+          <Route path='blog-titles' element={<BlogTitles/>}/>
+          <Route path='generate-images' element={<GenerateImages/>}/>
+          <Route path='remove-background' element={<RemoveBackground/>}/> 
+          <Route path='remove-object' element={<RemoveObjects/>}/> 
+          <Route path='review-resume' element={<ReviewResume/>}/> 
+          <Route path='community' element={<Community/>}/> 
+        </Route>
+      </Routes>
+
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/pages/GenerateImages.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GenerateImages = () => {
+  return (
+    <h1>GenerateImages</h1>
+  )
+}
+
+export default GenerateImages
