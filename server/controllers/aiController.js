ffecf9875126ef9c39d684abbad877fb6a18520a
aarush005 <astaarush005@gmail.com>
2025-10-22 14:22:42 +0530

    Create Background Removal api

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -150,3 +150,42 @@ export const generateImage = async (req, res) => {
         res.json({ success: false, message:error.message })
     }
 }
+
+
+
+
+
+// Remove Image API
+
+
+
+export const removeImageBackground = async (req, res) => {
+    try {
+        const { userId } = req.auth();
+        const {image} = req.file;
+        const plan = req.plan;
+
+
+        if (plan !== 'premium') {
+            return res.json({ success: false, message: "This feature is available for premium subscriptions." })
+        }
+
+
+        const { secure_url } = await cloudinary.uploader.upload(image.path, {
+            transformation: [
+                {
+                    effect: 'background_removal',
+                    background_removal : 'remove_the_background'
+                }
+            ]
+        })
+
+        await sql` INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId}, 'Remove background from image', ${secure_url}, 'image',)`;
+
+        res.json({ success: true, content: secure_url })
+
+    } catch (error) {
+        console.log(error.message)
+        res.json({ success: false, message:error.message })
+    }
+}
