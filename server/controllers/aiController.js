1ad8872a6aec847765c25bbc5b2c95d004aea869
aarush005 <astaarush005@gmail.com>
2025-10-22 14:30:17 +0530

    Create object remove api

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -157,8 +157,6 @@ export const generateImage = async (req, res) => {
 
 // Remove Image API
 
-
-
 export const removeImageBackground = async (req, res) => {
     try {
         const { userId } = req.auth();
@@ -183,6 +181,41 @@ export const removeImageBackground = async (req, res) => {
         await sql` INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId}, 'Remove background from image', ${secure_url}, 'image',)`;
 
         res.json({ success: true, content: secure_url })
+
+    } catch (error) {
+        console.log(error.message)
+        res.json({ success: false, message:error.message })
+    }
+}
+
+
+
+
+// Object Remove API
+
+export const removeImageObject = async (req, res) => {
+    try {
+        const { userId } = req.auth();
+        const { object } = req.body();
+        const {image} = req.file;
+        const plan = req.plan;
+
+
+        if (plan !== 'premium') {
+            return res.json({ success: false, message: "This feature is available for premium subscriptions." })
+        }
+
+
+        const { public_id } = await cloudinary.uploader.upload(image.path)
+
+      const imageUrl =  cloudinary.url(public_id, {
+            transformation:[{effect: `gen_remove: ${object}`}],
+            resource_type : 'image'
+        })
+
+        await sql` INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId}, ${ `Removed ${object} from image`}, ${imageUrl}, 'image',)`;
+
+        res.json({ success: true, content: imageUrl})
 
     } catch (error) {
         console.log(error.message)
